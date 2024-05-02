5aff995b42803081d6c11234420884a3279a0c8b
anais <mouelhi.chacha@gmail.com>
2024-05-02 16:55:40 +0200

    3 commit

--- app.js
+++ app.js
@@ -14,12 +14,21 @@ app.use((req, res, next) => {
     next();
 });
 
-// Proxy pour rediriger les demandes vers l'API OpenWeather
+// Proxy pour rediriger les demandes météorologiques vers l'API OpenWeather
 app.use('/weather', createProxyMiddleware({
     target: 'https://api.openweathermap.org',
     changeOrigin: true,
     pathRewrite: {
         '^/weather': '/data/2.5/weather'
+    }
+}));
+
+// Proxy pour rediriger les demandes d'indice de pollution de l'air vers l'API OpenWeather
+app.use('/air_pollution', createProxyMiddleware({
+    target: 'https://api.openweathermap.org',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/air_pollution': '/data/2.5/air_pollution/forecast'
     }
 }));
 
