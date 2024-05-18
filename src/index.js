e4b606be7abb5724c98f9eb2b8016c27e885adbc
anais <mouelhi.chacha@gmail.com>
2024-05-18 18:38:38 +0200

    Amélioration vercel

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import { connectToDatabase } from './lib/mongodb.js';
 import historyRoutes from './routes/historyRoutes.js';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-var whitelist = ['http://localhost:59444',
- 'http://localhost:3000',
-  'https://back-angular-app.vercel.app/',
-   'https://chainez-mouelhi.github.io/Angular_localisation_app/'];
+const whitelist = [
+  'http://localhost:59444',
+  'http://localhost:3000',
+  'https://back-angular-app.vercel.app',
+  'https://chainez-mouelhi.github.io',
+  'https://chainez-mouelhi.github.io/Angular_localisation_app/'
+];
 
-var corsOptions = {
+const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   }
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
 app.use('/api/history', historyRoutes);
 
 const startServer = async () => {
@@ -34,21 +35,19 @@ const startServer = async () => {
     console.log("Connected to MongoDB");
 
     app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+      console.log(`Server running on port ${port}`);
     });
 
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
-    process.exit(1); // Arrête le processus en cas d'échec de la connexion
+    process.exit(1);
   }
 };
 
-// Middleware pour gérer les erreurs 404
 app.use((req, res, next) => {
   res.status(404).send("Sorry, can't find that!");
 });
 
-// Middleware pour gérer les erreurs 500
 app.use((err, req, res, next) => {
   console.error(err.message);
   console.error(err.stack);
