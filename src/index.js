177a7ac86e31968a8abe57b29690175bed7a23c4
anais <mouelhi.chacha@gmail.com>
2024-05-20 23:30:44 +0100

    Amélioration du code HTML

--- src/index.js
+++ src/index.js
@@ -15,48 +15,47 @@ const whitelist = [
   'https://chainez-mouelhi.github.io/Angular_localisation_app'
 ];
 
-const corsOptions = {
+var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'))
     }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  }
 };
 
-// Use CORS middleware
 app.use(cors(corsOptions));
 
-// Handle preflight requests
-app.options('*', cors(corsOptions));
-
 app.use(express.json());
+
 app.use('/api/history', historyRoutes);
+
 
 const startServer = async () => {
   try {
+
     await connectToDatabase();
     console.log("Connected to MongoDB");
 
     app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+        console.log(`Server running on port ${port}`);
     });
 
   } catch (error) {
+
     console.error("Failed to connect to MongoDB", error);
-    process.exit(1);
+    process.exit(1); // Arrête le processus en cas d'échec de la connexion
+
   }
 };
 
-// Catch-all 404 handler
+// Middleware pour gérer les erreurs 404
 app.use((req, res, next) => {
   res.status(404).send("Sorry, can't find that!");
 });
 
-// Global error handler
+// Middleware pour gérer les erreurs 500
 app.use((err, req, res, next) => {
   console.error(err.message);
   console.error(err.stack);
